c2a79a29075abcb4e96096d42b91cc8463f38c52
austin.may1 <austin.may@protonmail.com>
2023-04-03 12:25:28 -0400

    Assignment_2

---
+++ Assignment_2/src/main/java/org/example/Employee.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private double monthlySalary;
+
+    public Employee(String firstName, String lastName, double monthlySalary) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setMonthlySalary(monthlySalary);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(double monthlySalary) {
+            this.monthlySalary = monthlySalary;
+    }
+
+    public double getYearlySalary() { return monthlySalary * 12; }
+
+    public void giveRaise(double amount) {
+        double raiseAmount = amount * getMonthlySalary();
+        this.monthlySalary = getMonthlySalary() + raiseAmount;
+    }
+}
---
+++ Assignment_2/src/main/java/org/example/EmployeeTest.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee emp_1 = new Employee("Mike", "Jones", 3500);
+        Employee emp_2 = new Employee("John", "Doe", 4000);
+        Employee emp_3 = new Employee("Jane", "Smith", 3800);
+        Employee emp_4 = new Employee("Jerry", "Seinfeld", 4200);
+        Employee emp_5 = new Employee("Jeniffer", "Anniston", 6000);
+
+        System.out.println("==========Employees==========");
+
+        System.out.printf("First Name: %s , Last Name: %s, Yearly Salary: $%.2f\n", emp_1.getFirstName(), emp_1.getLastName(), emp_1.getYearlySalary());
+        System.out.printf("First Name: %s , Last Name: %s, Yearly Salary: $%.2f\n", emp_2.getFirstName(), emp_2.getLastName(), emp_2.getYearlySalary());
+        System.out.printf("First Name: %s , Last Name: %s, Yearly Salary: $%.2f\n", emp_3.getFirstName(), emp_3.getLastName(), emp_3.getYearlySalary());
+        System.out.printf("First Name: %s , Last Name: %s, Yearly Salary: $%.2f\n", emp_4.getFirstName(), emp_4.getLastName(), emp_4.getYearlySalary());
+        System.out.printf("First Name: %s , Last Name: %s, Yearly Salary: $%.2f\n", emp_5.getFirstName(), emp_5.getLastName(), emp_5.getYearlySalary());
+
+        Scanner scanner = new Scanner(System.in);
+        double pct;
+
+        System.out.println("\nLets give some raises! Enter a raise amount between 0.02-0.06 for each employee.\n");
+
+        System.out.printf("Enter a raise amount for %s: ", emp_1.getFirstName());
+        pct = scanner.nextDouble();
+        emp_1.giveRaise(pct);
+        System.out.printf("%s's new yearly salary: $%.2f\n", emp_1.getFirstName(), emp_1.getYearlySalary());
+
+        System.out.printf("\nEnter a raise amount for %s: ", emp_2.getFirstName());
+        pct = scanner.nextDouble();
+        emp_2.giveRaise(pct);
+        System.out.printf("%s's new yearly salary: $%.2f\n", emp_2.getFirstName(), emp_2.getYearlySalary());
+
+        System.out.printf("\nEnter a raise amount for %s: ", emp_3.getFirstName());
+        pct = scanner.nextDouble();
+        emp_3.giveRaise(pct);
+        System.out.printf("%s's new yearly salary: $%.2f\n", emp_3.getFirstName(), emp_3.getYearlySalary());
+
+        System.out.printf("\nEnter a raise amount for %s: ", emp_4.getFirstName());
+        pct = scanner.nextDouble();
+        emp_4.giveRaise(pct);
+        System.out.printf("%s's new yearly salary: $%.2f\n", emp_4.getFirstName(), emp_4.getYearlySalary());
+
+        System.out.printf("\nEnter a raise amount for %s: ", emp_5.getFirstName());
+        pct = scanner.nextDouble();
+        emp_5.giveRaise(pct);
+        System.out.printf("%s's new yearly salary: $%.2f\n", emp_5.getFirstName(), emp_5.getYearlySalary());
+
+        System.out.println("\nWay to go! Here is a summary of your changes:\n");
+
+        System.out.printf("First Name: %s , Last Name: %s, Yearly Salary: $%.2f\n", emp_1.getFirstName(), emp_1.getLastName(), emp_1.getYearlySalary());
+        System.out.printf("First Name: %s , Last Name: %s, Yearly Salary: $%.2f\n", emp_2.getFirstName(), emp_2.getLastName(), emp_2.getYearlySalary());
+        System.out.printf("First Name: %s , Last Name: %s, Yearly Salary: $%.2f\n", emp_3.getFirstName(), emp_3.getLastName(), emp_3.getYearlySalary());
+        System.out.printf("First Name: %s , Last Name: %s, Yearly Salary: $%.2f\n", emp_4.getFirstName(), emp_4.getLastName(), emp_4.getYearlySalary());
+        System.out.printf("First Name: %s , Last Name: %s, Yearly Salary: $%.2f\n", emp_5.getFirstName(), emp_5.getLastName(), emp_5.getYearlySalary());
+
+        System.out.println("\nHave a nice day!");
+    }
+}
